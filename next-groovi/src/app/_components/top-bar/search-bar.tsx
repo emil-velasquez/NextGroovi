c438a75af845f8f1018704a965baf1481e22dcac
Emil Velasquez <evelas10@utexas.edu>
2023-11-04 17:59:26 -0500

    #6: Basics of a search bar

---
+++ next-groovi/src/app/_components/top-bar/search-bar.tsx
@@ -0,0 +1,30 @@
+"use client"
+import styles from "../../_styles/top-bar/SearchBar.module.scss";
+
+import React, { useState } from "react"
+import { useRouter } from "next/navigation";
+
+import Button from "@/sdk/Button/button";
+import { BsSearch } from "react-icons/bs";
+
+export default function SearchBar() {
+    const [searchText, setSearchText] = useState("");
+    const router = useRouter();
+
+    function onSubmit() {
+        router.push(`/search?q=${searchText}`);
+    }
+
+    return (
+        <div className={styles.searchBarContainer}>
+            <input type="text" className={styles.searchText} placeholder="Search" onChange={e => setSearchText(e.target.value)} />
+            <div className={styles.searchButtonContainer}>
+                <Button onClick={onSubmit} priority="secondary">
+                    <div className={styles.searchButtonWrapper}>
+                        <BsSearch className={styles.searchButton} />
+                    </div>
+                </Button>
+            </div>
+        </div>
+    )
+}
