9bb73137968f40451fe38d0823da0dc46d74644f
Emil Velasquez <evelas10@utexas.edu>
2023-11-04 10:53:42 -0500

    Closes #12: Created base playlist nav bar

--- next-groovi/src/app/_components/navigation-bar/playlist-bar.tsx
+++ next-groovi/src/app/_components/navigation-bar/playlist-bar.tsx
@@ -1,7 +1,28 @@
+import styles from "../../_styles/navigation-bar/ActionBar.module.scss";
+
+import Card from "@/sdk/Card/card";
+import Hover from "@/sdk/Hover/hover";
+import NavLink from "@/sdk/NavLink/navlink";
+import { BsCollectionPlayFill, BsFillPlusSquareFill } from "react-icons/bs";
+
 export default function PlaylistBar() {
     return (
-        <div>
-
-        </div>
+        <Card>
+            <div className={styles.mainActionBar}>
+                <div className={styles.gap} />
+                <Hover hoverText="Playlists" hoverDirection="right">
+                    <div className={styles.buttonContainer}>
+                        <BsCollectionPlayFill className={styles.mainActionButton} />
+                    </div>
+                </Hover>
+                <div className={styles.optionContainer}>
+                    <NavLink address="/playlist" hoverText="Create new playlist" hoverDirection="right">
+                        <div className={styles.buttonContainer}>
+                            <BsFillPlusSquareFill className={styles.mainActionButton} />
+                        </div>
+                    </NavLink>
+                </div>
+            </div>
+        </Card>
     )
 }
