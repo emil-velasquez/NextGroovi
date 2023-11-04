d6fbe0a0eef3f4c0389402e121e205f081c91abb
Emil Velasquez <evelas10@utexas.edu>
2023-11-03 22:48:22 -0500

    Created preliminary bar to hold primary nav actions

--- next-groovi/src/app/(home)/components/navigation-bar/main-action-bar.tsx
+++ next-groovi/src/app/(home)/components/navigation-bar/main-action-bar.tsx
@@ -1,20 +1,47 @@
 "use client"
 
+import NavLink from "@/sdk/NavLink/navlink"
 import styles from "../../styles/navigation-bar/MainActionBar.module.scss"
-import Button from "@/sdk/Button/button"
 
-export default function MainActionBar() {
-    function test() {
-        console.log("here");
-    }
+import { BsFillHouseFill, BsSuitHeartFill, BsFire } from "react-icons/bs"
+import Card from "@/sdk/Card/card"
+
+interface MainActionOptionProps {
+    children: React.ReactNode,
+    address: string,
+    hoverText: string
+}
+
+function MainActionOption(props: MainActionOptionProps) {
+    const { children, address, hoverText } = props;
 
     return (
-        <div>
-            <Button onClick={test} priority="secondary" hoverText="test">
-                <div className={styles.test}>
-                    test
+        <div className={styles.optionContainer}>
+            <NavLink address={address} hoverText={hoverText} hoverDirection="right">
+                <div className={styles.buttonContainer}>
+                    {children}
                 </div>
-            </Button>
+            </NavLink>
         </div>
+    )
+}
+
+export default function MainActionBar() {
+    return (
+        <Card>
+            <div className={styles.mainActionBar}>
+                <div className={styles.gap} />
+                <MainActionOption address="/" hoverText="Home">
+                    <BsFillHouseFill className={styles.mainActionButton} />
+                </MainActionOption>
+                <MainActionOption address="/playlist/1" hoverText="Trending">
+                    <BsFire className={styles.mainActionButton} />
+                </MainActionOption>
+                <MainActionOption address="/playlist/2" hoverText="Favorites">
+                    <BsSuitHeartFill className={styles.mainActionButton} />
+                </MainActionOption>
+                <div className={styles.gap} />
+            </div>
+        </Card>
     )
 }
