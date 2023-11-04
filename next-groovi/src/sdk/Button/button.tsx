ea4df74a088524229da3fef1ca990b90ef97f2d8
Emil Velasquez <evelas10@utexas.edu>
2023-11-03 20:22:15 -0500

    Updated button to use the Hover wrapper

--- next-groovi/src/sdk/Button/button.tsx
+++ next-groovi/src/sdk/Button/button.tsx
@@ -1,33 +1,24 @@
 "use client"
 
+import Hover from "../Hover/hover";
 import styles from "./Button.module.scss"
-import React, { useState } from "react";
 
 interface ButtonProps {
     onClick: () => any,
     priority: "primary" | "secondary",
-    showHover: boolean,
     hoverText?: string,
-    hoverDirection?: "left" | "right"
     children: React.ReactNode
 }
 
 export default function Button(props: ButtonProps) {
-    const { onClick, priority, showHover, hoverText, hoverDirection, children } = props;
+    const { onClick, priority, children, hoverText } = props;
     const buttonStyle = priority == "primary" ? styles.primaryButton : styles.secondaryButton;
-    const hoverStyle = hoverDirection == "left" ? styles.hoverTextLeft : styles.hoverTextRight;
-
-    const [isHovering, setIsHovering] = useState(false);
 
     return (
-        <div onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)} className={styles.buttonContainer}>
+        <Hover hoverText={hoverText} hoverDirection="right">
             <button onClick={onClick} className={buttonStyle}>
                 {children}
             </button>
-            {showHover && isHovering &&
-                <div className={hoverStyle}>
-                    <text>{hoverText}</text>
-                </div>}
-        </div>
+        </Hover>
     )
 }
