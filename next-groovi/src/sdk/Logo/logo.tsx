2316d966a0e1f484c21108a42380efd749ef7c8a
Emil Velasquez <evelas10@utexas.edu>
2023-12-19 16:24:59 -0600

    Moved logo to be an sdk component

---
+++ next-groovi/src/sdk/Logo/logo.tsx
@@ -0,0 +1,24 @@
+import styles from "./Logo.module.scss"
+import Card from "@/sdk/Card/card"
+
+import { BsDiscFill } from "react-icons/bs"
+
+interface LogoProps {
+    size: number
+}
+
+export default function Logo(props: LogoProps) {
+    const { size } = props;
+    const logoStyle = { width: size + "rem", height: size + "rem" }
+
+    const containerSize = size + 1;
+    const containerSizeStyle = { width: containerSize + "rem", height: containerSize + "rem" }
+
+    return (
+        <Card>
+            <div className={styles.logoContainer} style={containerSizeStyle}>
+                <BsDiscFill className={styles.logo} style={logoStyle} />
+            </div>
+        </Card>
+    )
+}
