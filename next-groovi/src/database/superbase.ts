84bc4dc96e28ecbe08d98cd9faccff24257667c4
Emil Velasquez <evelas10@utexas.edu>
2023-12-20 00:45:15 -0600

    updated supabase to be client constructors

--- next-groovi/src/database/superbase.ts
+++ next-groovi/src/database/superbase.ts
@@ -1,43 +1,47 @@
 import { createBrowserClient, createServerClient, type CookieOptions } from "@supabase/ssr";
-import { cookies } from "next/headers"
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 
 // for use in client components
-export const clientSupabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
-
-const cookieStore = cookies()
+export function createClientSupabase() {
+    return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+}
 
 // for use in server components (only has read access to cookies)
-export const serverSupabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-        cookies: {
-            get(name: string) {
-                return cookieStore.get(name)?.value
+export function createServerSupabase(cookieStore: ReadonlyRequestCookies) {
+    return createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                get(name: string) {
+                    return cookieStore.get(name)?.value
+                },
             },
-        },
-    }
-)
+        }
+    )
+}
 
 // for use in API routes (ie GET, POST, etc.)
 // also for server actions
-export const routeSupabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-        cookies: {
-            get(name: string) {
-                return cookieStore.get(name)?.value
+export function createRouteSupabase(cookieStore: ReadonlyRequestCookies) {
+    return createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                get(name: string) {
+                    return cookieStore.get(name)?.value
+                },
+                set(name: string, value: string, options: CookieOptions) {
+                    cookieStore.set({ name, value, ...options })
+                },
+                remove(name: string, options: CookieOptions) {
+                    cookieStore.set({ name, value: '', ...options })
+                },
             },
-            set(name: string, value: string, options: CookieOptions) {
-                cookieStore.set({ name, value, ...options })
-            },
-            remove(name: string, options: CookieOptions) {
-                cookieStore.set({ name, value: '', ...options })
-            },
-        },
-    }
-)
+        }
+    )
+}
